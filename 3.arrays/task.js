d1a24c28cc486b2748b2b3ae9014253e34b2d0c2
Artem18 <urart88@mail.ru>
2022-05-22 10:03:19 +0400

    Первые 3 задания верны и приняты

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,13 @@
+//Задание 1
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  let result = (arr1.length === arr2.length && arr1.every((item, idx) => item === arr2[idx]));
+    return result;
 }
 
+//Задание 2
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  let resultArr = arr.filter((plus) => plus > 0)
+    .filter ((multiplicity) => multiplicity % 3 === 0)
+    .map ((multiplication) => multiplication * 10)
+   return resultArr; // array
 }
